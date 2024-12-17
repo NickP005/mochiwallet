59d30fba3ef0ad1c1eed0702585f94c194261302
awagl <anujwagle@gmail.com>
2024-12-17 02:52:51 -0500

    change theme toggle colors

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -4,15 +4,19 @@ import { useTheme } from "@/components/theme-provider"
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
+  const isDark = theme === 'dark'
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => setTheme(isDark ? "light" : "dark")}
     >
-      <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      {isDark ? (
+        <Moon className="h-5 w-5 text-blue-200" />
+      ) : (
+        <Sun className="h-5 w-5 text-yellow-500" />
+      )}
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
