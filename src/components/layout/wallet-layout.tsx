f6f7b93140440a7cb67b829933f53b7057cc988e
awagl <anujwagle@gmail.com>
2025-01-17 02:51:32 -0500

    remove theme toggle from header

--- src/components/layout/wallet-layout.tsx
+++ src/components/layout/wallet-layout.tsx
@@ -52,7 +52,6 @@ export function WalletLayout({
           </div>
         </div>
         <div className="flex items-center ">
-        <ThemeToggle />
           {isExtension && (
             <Tooltip>
               <TooltipTrigger asChild>
